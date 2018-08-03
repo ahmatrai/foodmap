1096ee2880aff88c455fd6857befb7a6cd50f859
Ana H Matrai <ahmatrai@gmail.com>
2018-08-03 17:18:13 -0300

    html splash jquery

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-// Add your JavaScript
+$(document).ready(function() {
+    $('.start').delay('3000').fadeOut('slow');
+    $('.principal').hide();
+    $('.principal').delay('3000').fadeIn('slow');
+});
+
