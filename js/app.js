3f71280f9dd5bdd67c7a89fe8c7c19f236030042
Ana H Matrai <ahmatrai@gmail.com>
2018-08-05 21:13:03 -0300

    filtra restaurantes

--- js/app.js
+++ js/app.js
@@ -1,6 +1,25 @@
 $(document).ready(function() {
-    $('.start').delay('3000').fadeOut('slow');
+    $('.start').delay('2000').fadeOut('slow');
     $('.principal').hide();
-    $('.principal').delay('3000').fadeIn('slow');
-});
+    $('.principal').delay('2800').fadeIn('slow');
+
+
+    $('.filtrar').click(function () {
+      var inputValue = $('.tipo').val();
+  
+      $.getJSON(restaurantes).each(function( ) {
+        if($(this).text() !== inputValue) {
+          $(this).fadeOut('slow');
+        }
+      });
+    })
+  
+    $('.tipo').on('input', function () {
+      if($(this).val() === "") {
+        $( "restaurantes" ).each(function( ) {
+          $(this).fadeIn('slow')
+        });
+      }
+    })
+  });
 
